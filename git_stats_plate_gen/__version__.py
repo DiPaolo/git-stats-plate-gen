38dd7ef6de6ec0820844f85d87c48640a7c26677
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2023-08-28 11:43:06 +0700

    add version to config

--- git_stats_plate_gen/__version__.py
+++ git_stats_plate_gen/__version__.py
@@ -1,4 +1,5 @@
-__version__ = "0.0.0"
+# see config for getting version
+# __version__ = "0.0.0"
 __author__ = "Pavel 'DiPaolo' Ditenbir"
 __author_email__ = "pavel.ditenbir@gmail.com"
 __copyright__ = "Copyright Pavel 'DiPaolo' Ditenbir"
