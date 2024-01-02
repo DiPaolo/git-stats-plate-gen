ba058775300a67e1943abb186bac04a7a9ac8692
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2024-01-02 13:31:03 +0700

    change last name: Ditenbir -> Dittenbier

--- git_stats_plate_gen/__version__.py
+++ git_stats_plate_gen/__version__.py
@@ -1,5 +1,5 @@
 # see config for getting version
 # __version__ = "0.0.0"
-__author__ = "Pavel 'DiPaolo' Ditenbir"
+__author__ = "Pavel 'DiPaolo' Dittenbier"
 __author_email__ = "pavel.ditenbir@gmail.com"
-__copyright__ = "Copyright Pavel 'DiPaolo' Ditenbir"
+__copyright__ = "Copyright Pavel 'DiPaolo' Dittenbier"
